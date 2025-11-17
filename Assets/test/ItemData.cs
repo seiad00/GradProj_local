81fa596b5355e655e779865e7f400ea27c48735b
seiad00 <tlsdnjsdud51@naver.com>
2025-11-17 12:48:30 +0900

    251117

---
+++ Assets/test/ItemData.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class ItemData
+{
+    public string id { get; set; }
+    public int quantity { get; set; }
+    /* 추가 필요 */
+}
--- Assets/test/StageManager.cs
+++ Assets/test/StageManager.cs
@@ -8,40 +8,9 @@ using UnityEditor.Build.Content;
 using SupanthaPaul;
 //using Cinemachine;
 
-// 아이템의 정보를 저장하는 클래스. 나중에 합칠 때 별도의 스크립트로 생성
-public class ItemData
-{
-    public string id { get; set; }
-    public int quantity { get; set; }
-    /* 추가 필요 */
-}
 
 public class StageManager : MonoBehaviour
 {
-    private int mapCount = 0;
-    //맵 생성을 위한 프리팹 지정
-    [Header("StartingMap")]
-    public GameObject StartingMap;
-    [Header("Candidate")]
-    public List<GameObject> OtherPreset;
-    [Header("Last")]
-    public GameObject LastMap;
-    //씬에서 동작할 플레이어 프리팹 지정
-    [Header("Player")]
-    public GameObject playerPrefab;
-    public Vector3 playerSpawn = new Vector3(0f, 0f, 0f);
-    //public CinemachineVirtualCamera vcam; // Cinemachine 문제 고쳐지면 주석 해제
-
-    //맵 생성 위치 결정을 위한 맵의 크기와 다음 맵 생성 위치변수. Y축만 고려. X축 필요 시 추가
-    [Header("Map Height")]
-    public float MapHeight = 16.5f; //실제 맵 크기에 따라 조정 필요
-    private float nextMapY = 0f;
-
-    //플레이어의 위치 추적을 위한 플레이어 지정
-    [Header("Player Transform")]
-    public Transform PlayerTransform;
-    [Header("Threshold")]
-    public float Threshold = 17f; // 맵 생성 타이밍 감지를 위한 임계값 설정
 
     //스크립트 참조
     [Header("Ref Player")]
@@ -80,70 +49,7 @@ public class StageManager : MonoBehaviour
 
     void Start()
     {
-        IsClear = false;
-        mapCount += 1;
-        /* Cinemachine 문제 고쳐지면 주석해제
-        //시작 맵 불러오기
-        if (StartingMap != null)
-        {
-            Instantiate(StartingMap, Vector3.zero, Quaternion.identity);
-            //다음 맵이 생성될 위치 조정
-            nextMapY = -MapHeight;
-            Debug.Log("StartingMap Called");
-        }
-        else Debug.LogError("StartingMap Required");
-
-        //플레이어 불러오기
-        if (playerPrefab != null)
-        {
-            GameObject SpawnedPlayer = Instantiate(playerPrefab, playerSpawn, Quaternion.identity);
-            Debug.Log("Player Spawned");
-            vcam.Follow = SpawnedPlayer.transform;
-            vcam.LookAt = SpawnedPlayer.transform;
-        }
-        else Debug.Log("Player Setting Required");
-        */
-        nextMapY = -MapHeight;
-        Debug.Log("nextMapY: " + nextMapY);
-    }
-
-    //함수 이름: SpawnRandomMap
-    //기능: 플레이어의 진행을 추적하여 다음에 생성될 맵을 랜덤하게 결정하는 함수
-    //파라미터: X
-    //반환값: X
-    public void SpawnRandomMap()
-    {
-        mapCount += 1;
-        //랜덤 프리셋이 없을 경우 리턴
-        if (OtherPreset.Count == 0)
-        {
-            Debug.Log("RandomMap Required"); return;
-        }
-        // 5번째 맵이 마지막
-        if(mapCount == 5)
-        {
-            Vector3 SpawnLoc = new Vector3(0, nextMapY, 0);
-            Instantiate(LastMap, SpawnLoc, Quaternion.identity);
-            Debug.Log("Last Map");
-            nextMapY = nextMapY - 1242148 * MapHeight;
-        }
-
-        else
-        {
-            //랜덤 프리셋의 맵 결정. 이후에 랜덤 관련 보정 필요
-            int MapIndex = Random.Range(0, OtherPreset.Count);
-            GameObject SelectedMap = OtherPreset[MapIndex];
-
-            //선택된 맵을 정해진 위치에 생성
-            Vector3 SpawnLoc = new Vector3(0, nextMapY, 0);
-            Instantiate(SelectedMap, SpawnLoc, Quaternion.identity);
-
-            //다음 맵이 생성될 위치 조정
-            nextMapY -= MapHeight;
-            Debug.Log("nextMapY: " + nextMapY);
-        }
-
- 
+        IsClear = false; CheckTrigger = false;
     }
 
     //함수 이름: ClearAssurance
@@ -176,7 +82,7 @@ public class StageManager : MonoBehaviour
     //함수 이름: GameOver
     //기능: 스테이지 실패를 알리는 함수
     //파라미터: X
-    //반환값: X (이벤트를 구독하는 스크립트에게 false 전달)
+    //반환값: X ( 이벤트를 구독하는 스크립트에게 bool IsClear ( 일반적으로 false ) 전달 )
     public void GameOver()
     {
         OnStageFail?.Invoke(IsClear);
@@ -187,7 +93,7 @@ public class StageManager : MonoBehaviour
     //함수 이름: StageClear
     //기능: 스테이지 클리어를 알리는 함수
     //파라미터: X
-    //반환값: X (이벤트를 구독하는 스크립트에게 true 전달)
+    //반환값: X ( 이벤트를 구독하는 스크립트에게 bool IsClear ( 일반적으로 true ) 전달 )
     public void StageClear(bool boss)
     {
         if (CheckTrigger && boss) OnStageClear?.Invoke(IsClear);
@@ -242,26 +148,13 @@ public class StageManager : MonoBehaviour
 
     void Update()
     {
-        //플레이어 설정이 안된 경우 리턴
-        if (PlayerTransform == null)
-        {
-            Debug.Log("Player Required");
-            return;
-        }
-        //플레이어가 맵의 특정 깊이에 도달하면 다음 맵을 불러옴
-        while(PlayerTransform.position.y <= nextMapY + Threshold)
-        {
-            SpawnRandomMap();
-            Debug.Log("nextMap Loaded");
-        }
-
         /*
-        //맵의 최상단으로 이동 시 탈출 시도임을 확인
-        if (PlayerTransform.position.y > -Threshold)
-        {
-            StageEscape(false); // 플레이를 중단하되, 획득한 아이템이 초기화
-        }
-        */
+         //맵의 최상단으로 이동 시 탈출 시도임을 확인
+         if (PlayerTransform.position.y > -Threshold)
+         {
+             StageEscape(false); // 플레이를 중단하되, 획득한 아이템이 초기화
+         }
+         */
     }
 
     /* 합칠 때 주석 해제
